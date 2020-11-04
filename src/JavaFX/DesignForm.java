bf191027179f92c0c108c9e95bfc6772b1133435
rekafekili <rekafekili@gmail.com>
2020-11-04 17:19:19 +0900

    Add Design Form JFX in Tutorials

--- src/JavaFX/DesignForm.java
+++ src/JavaFX/DesignForm.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -44,6 +47,20 @@ public class DesignForm extends Application {
 
         PasswordField passwordField = new PasswordField();
         grid.add(passwordField, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        btn.setOnAction((actionEvent) -> {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Sign in Button Pressed");
+        });
 
         primaryStage.show();
     }
